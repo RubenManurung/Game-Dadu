119c8bed1e5dcbd527e19ba96993ee93afd8fb34
RubenManurung <rubenmichael199@gmail.com>
2020-09-21 21:17:28 +0700

    Dice Game Web Based

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function playDice(){
+  let btnSound = new Audio("sounds/crash.mp3");
+  btnSound.play();
+  let numberOneToSix = Math.floor(Math.random()*6)+1;
+  let numberOneToSixV2 = Math.floor(Math.random()*6)+1;
+
+  if(numberOneToSix === numberOneToSixV2){
+    document.querySelector("h1").textContent = "Draw!";
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+numberOneToSix+".png");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+numberOneToSixV2+".png");
+  }else if(numberOneToSix > numberOneToSixV2){
+    document.querySelector("h1").textContent = "🏆Player 1 Win!";
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+numberOneToSix+".png");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+numberOneToSixV2+".png");
+  }else if(numberOneToSix < numberOneToSixV2){
+    document.querySelector("h1").textContent = "Player 2 Win!🏆";
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+numberOneToSix+".png");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+numberOneToSixV2+".png");
+  }else{
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice1.png");
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice1.png");
+  }
+}
